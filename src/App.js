51a330acc33cdec51a572c1914d46e6a5f0399e8
ExploTech <94236541+OuafiSoumia@users.noreply.github.com>
2023-11-26 21:14:48 +0000

    feature/medcinList

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
-import Nav from './components/Nav';
-import Service1 from './components/Service1';
+//import Nav from './components/Nav';
+//import Service1 from './components/Service1';
+import SpaceMedcin from './pages/SpaceMedcin'
 function App() {
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
-      <Header />
+      {/* <Header />
       <Banner />
       <Nav />
-      <Service1 />
+      <Service1 /> */}
+      <SpaceMedcin />
     </div>
   );
 }
---
+++ src/pages/SpaceMedcin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../components/medcin/Banner';
+import Header from '../components/Header';
+
+function SpaceMedcin() {
+  return (
+    <div>
+        <Header/>
+        <Banner />
+    </div>
+  );
+}
+
+export default SpaceMedcin;
